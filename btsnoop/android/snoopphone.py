e223515dc92c8fa9bd6eafc7319b566680cfa68f
Tomas Nilsson <tnilsson.x@gmail.com>
2015-04-27 10:10:48 +0200

    Add bt_stack.conf location

--- btsnoop/android/snoopphone.py
+++ btsnoop/android/snoopphone.py
@@ -1,24 +1,75 @@
 import os
 import tempfile
+import ConfigParser
 
 from phone import Phone
 
 
-BTSNOOP_FILE = 'btsnoop_hci.log'
-BTSNOOP_SRC = os.path.join('/sdcard', BTSNOOP_FILE)
+BTSTACK_CONFIG_FILE = 'bt_stack.conf'
+BTSTACK_CONFIG_PATH = os.path.join('/', 'etc', 'bluetooth', BTSTACK_CONFIG_FILE)
+
+BTSNOOP_FALLBACK_FILE = 'btsnoop_hci.log'
+BTSNOOP_FALLBACK_PATH = os.path.join('/', 'sdcard', BTSNOOP_FALLBACK_FILE)
 
 
 class SnoopPhone(Phone):
 
     def __init__(self, serial=None):
         super(SnoopPhone, self).__init__(serial=serial)
-        self._src = BTSNOOP_SRC
-        tmp_dir = tempfile.mkdtemp()
-        self._dst = os.path.join(tmp_dir, BTSNOOP_FILE)
+        self._tmp_dir = tempfile.mkdtemp()
 
     def pull_btsnoop(self, dst=None):
+        
+        btsnoop_path, btsnoop_file = self._locate_btsnoop()
+
         if not dst:
-            dst = self._dst
-        ret = super(SnoopPhone, self).pull(self._src, dst)
-        print ret
-        return dst
+            dst = os.path.join(self._tmp_dir, btsnoop_file)
+
+        ret = super(SnoopPhone, self).pull(btsnoop_path, dst)
+        if ret[0] == 0:
+            print ret[1] + " " + dst 
+            return dst
+        else:
+            return None
+
+    def _locate_btsnoop(self):
+        tmp_config_path = self._pull_btconfig()
+        config = self._parse_btconfig(tmp_config_path)
+        try:
+            btsnoop_path = config['btsnoopfilename']
+            return btsnoop_path, os.path.basename(btsnoop_path)
+        except:
+            return BTSNOOP_FALLBACK_PATH, BTSNOOP_FALLBACK_FILE
+
+    def _parse_btconfig(self, path):
+        if not os.path.exists(path):
+            raise ValueError("Failed to read bt_stack.conf")
+
+        # Needed because ConfigParser won't work without at least one header
+        class DummyHeaderFile(object):
+            def __init__(self, fp):
+                self.fp = fp
+                self.dummy = '[dummy]\n'
+
+            def readline(self):
+                if self.dummy:
+                    try: 
+                        return self.dummy
+                    finally: 
+                        self.dummy = None
+                else: 
+                    return self.fp.readline()
+        
+        # Parse key/values
+        parser = ConfigParser.SafeConfigParser()
+        with open(path, 'r') as f:
+            parser.readfp(DummyHeaderFile(f))
+            return dict(parser.items('dummy'))
+
+    def _pull_btconfig(self):
+        dst = os.path.join(self._tmp_dir, BTSTACK_CONFIG_FILE)
+        retcode, out = super(SnoopPhone, self).pull(BTSTACK_CONFIG_PATH, dst)
+        if retcode == 0:
+            return dst
+        else:
+            raise ValueError("Failed to pull bt_stack.conf")
